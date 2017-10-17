cff2b66d943053384d99ff8dd7815c53babca8c8
mauricebuiten <maubui@gmail.com>
2017-10-17 16:19:32 +0200

    Add Users to Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017135733) do
+ActiveRecord::Schema.define(version: 20171017141633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20171017135733) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -61,4 +63,5 @@ ActiveRecord::Schema.define(version: 20171017135733) do
   end
 
   add_foreign_key "cart_items", "products"
+  add_foreign_key "orders", "users"
 end
