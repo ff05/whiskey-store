b9e6014b5d5f2254828d11171c5ad0d411238104
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-17 16:08:14 +0200

    association test user and order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "association with user" do
+    let(:user) { create :user }
+
+    it { is_expected.to belong_to :user}
+  end
 end
