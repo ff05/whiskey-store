db35fbfa3d1650074ea20c7ce52028114907af0c
mauricebuiten <maubui@gmail.com>
2017-10-17 21:18:46 +0200

    Rename cart_item, Add Create and Index action to CartItem controller

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,20 @@
 class CartItemsController < ApplicationController
-  def show
+
+  def index
+    @cart_items = CartItem.all
   end
 
   def new
   end
 
   def create
+    @product = Product.find(params[:product_id])
+    @cart_item = CartItem.new
+    @cart_item.product_id = @product
+    if @cart_item.save
+      redirect_to cart_items_path
+    else redirect_to products_path
+    end
   end
 
   def edit
