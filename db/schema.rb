0cf5bb0c78bce6e62ad483c3587cf49549abe042
mauricebuiten <maubui@gmail.com>
2017-10-17 16:02:35 +0200

    Modifie Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017134017) do
+ActiveRecord::Schema.define(version: 20171017135733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 20171017134017) do
   create_table "cart_items", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -57,4 +60,5 @@ ActiveRecord::Schema.define(version: 20171017134017) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "products"
 end
