d5ee5a1008cb0d804ac3d4e38c8c861d8a0eaa20
brunadafonseca <brunadafonseca@gmail.com>
2017-10-17 15:57:13 +0200

    Create controllers and views: Profile and Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  get 'profiles/new'
+
+  get 'profiles/create'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
+  get 'profiles/destroy'
+
+  get 'products/index'
+
+  get 'products/show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
