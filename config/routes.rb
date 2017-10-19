f788b58a17bec9312a75aefae898d539b3c4c26e
frankfil <frank.filius@gmail.com>
2017-10-19 16:50:17 +0200

    Add Profiles route + Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+  resources :profiles
   resources :order
   resources :cart_items
   resources :products do
