d8e1a109f9f93a69800c3f484e72934c92e34b84
floris09 <florismeininger@gmail.com>
2017-10-20 15:34:07 +0200

    making the sessions work

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,15 +1,28 @@
 class CartsController < ApplicationController
+  def index
+    @cart = []
+  end
+
+  def add_to_cart
+    current_cart.each do |item|
+      @cart.push(item.name)
+      @cart
+    end
+  end
+
   def show
-    @cart = Cart.find(params[:id])
-    @products = @cart.products
+    @cart = current_cart
   end
 
   def edit
     @cart = Cart.find(params[:id])
   end
 
+  def update
+    @product = Product.find(params[:id])
+    current_cart << @product.id
 
-
-  def edit
+    session[:cart] = current_cart
+  end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,11 @@
 class Cart < ApplicationRecord
   has_many :cart_items
 
+  def cart_items
+    cart_items = []
+    session[:cart].each do |item|
+      cart_items << Product.find(item)
+    end
+    cart_items
+  end
 end
